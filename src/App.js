9d7638e22a5897b473021389cf566ed7d1279fc9
Connor Christensen <connor.christensen@uvu.edu>
2023-08-09 11:40:34 -0600

    Recipe App set up and API is hooked up

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { FaSearch } from "react-icons/fa";
 
 function App() {
+  // CALLS THE RECIPE FUNCTION IF WE PRESS ENTER
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      getRecipes()
+    }};
+    // FUNCTION TO GET RECIPES ON SEARCH
+  async function getRecipes() {
+    let searchedRecipe = document.getElementById('search').value
+    console.log(searchedRecipe)
+
+    fetch(`https://api.spoonacular.com/recipes/search?query=${searchedRecipe}&apiKey=592c176aa0f74669aacd253abca7bcbc`)
+      .then(response => response.json())
+      .then(data => console.log(data));
+  }
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>WheatRecipes</h1>
+        <input type='text' placeholder='Find a Recipe...' id='search' className='recipeFinderSearch' onKeyDown={handleKeyDown}/>
+        <FaSearch className='searchIcon' onClick={getRecipes}/>
       </header>
     </div>
   );
